3143449eee6e53644450b066d8a1fd7972cf2054
Kyle Murray <kdevlinmurray@gmail.com>
2020-02-23 15:45:50 -0800

    Updates

---
+++ precache-manifest.29a5b33e2e12ca919ca83ff5aabcb445.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02614aaad5a7bc4236d6f4544c22d47c",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "0af67dd296d751f0d224",
+    "url": "/flashcards/static/js/2.39db46d3.chunk.js"
+  },
+  {
+    "revision": "f9c34e79dacdea5d9f16",
+    "url": "/flashcards/static/js/3.f08dd6d5.chunk.js"
+  },
+  {
+    "revision": "fab2a3354daac51efba5",
+    "url": "/flashcards/static/js/4.68b61d85.chunk.js"
+  },
+  {
+    "revision": "b2b692ef063080de2d97",
+    "url": "/flashcards/static/js/main.57409792.chunk.js"
+  },
+  {
+    "revision": "c8f4fbe15e72e565349a",
+    "url": "/flashcards/static/js/runtime-main.409684ea.js"
+  },
+  {
+    "revision": "c04a1212d1bf13d49c947385bfc9eef3",
+    "url": "/flashcards/static/media/drink1.c04a1212.svg"
+  },
+  {
+    "revision": "5f9bc2b7c2ab32c7da604b3636d3dcde",
+    "url": "/flashcards/static/media/nofile-picture.5f9bc2b7.jpg"
+  },
+  {
+    "revision": "9928130ab94e6f805d41174ec1c4d834",
+    "url": "/flashcards/static/media/sprite.9928130a.svg"
+  }
+]);
